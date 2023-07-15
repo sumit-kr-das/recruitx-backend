902bf2262ac9faeb9f229acaadc959b8b0ef2724
Shoyeb001 <shoyebjio3398@gmail.com>
2023-07-15 21:22:59 +0530

    user exprience schema added

--- src/model/userInfo.ts
+++ src/model/userInfo.ts
@@ -1,4 +1,17 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IUserExprience extends Document{
+    userId:string,
+    phone:string,
+    github:string,
+    linkedIn?:string
 }
+
+const UserExprienceSchema:Schema = new Schema({
+    userId:{type:String, required:true},
+    phone:{type:String, required:true},
+    github:{type:String, required:true},
+    linkedIn:{type:String}
+})
+
+export default model<IUserExprience>("UserExprience", UserExprienceSchema);
