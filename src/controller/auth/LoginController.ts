687bcc8eda13205f4d53ab3004fe199b37b6583f
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-15 12:51:05 +0530

    new response added

--- src/controller/auth/LoginController.ts
+++ src/controller/auth/LoginController.ts
@@ -1,83 +1,102 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
-import User from "../../model/User";
+import User from '../../model/User';
 import company from '../../model/company';
 import CustomErrorHandler from '../../services/customErrorHandeler';
 import bcrypt from 'bcrypt';
 import JwtService from '../../services/jwtServices';
+import roles from '../../services/roleService';
 
 const loginController = {
-    async userLogin(req:Request, res:Response, next:NextFunction){
+    async userLogin(req: Request, res: Response, next: NextFunction) {
         const userLoginSchema = Joi.object({
-            email:Joi.string().email().required(),
-            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required()
-        })
+            email: Joi.string().email().required(),
+            password: Joi.string()
+                .pattern(
+                    new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$'),
+                )
+                .required(),
+        });
 
-        const {error} = userLoginSchema.validate(req.body);
+        const { error } = userLoginSchema.validate(req.body);
 
-        if(error){
+        if (error) {
             next(error);
         }
 
-        const { email, password}:{email: string, password:string} = req.body;
+        const { email, password }: { email: string; password: string } =
+            req.body;
 
         try {
             const user = await User.findOne({ email });
-            if(!user){
+            if (!user) {
                 return next(CustomErrorHandler.wrongCredentials());
             }
             const matchPassword = await bcrypt.compare(password, user.password);
-            if(!matchPassword){
+            if (!matchPassword) {
                 return next(CustomErrorHandler.wrongCredentials());
             }
 
             /* compare access token */
             // console.log(user._id);
-            const access_token = JwtService.sign({ 
-                id: user._id, 
-                role: "user"
+            const access_token = JwtService.sign({
+                id: user._id,
+                role: 'user',
             });
 
             res.status(200).json({ access_token });
         } catch (error) {
             next(error);
         }
-
     },
 
-    async companyLogin(req:any, res:Response, next:NextFunction){
+    async companyLogin(req: any, res: Response, next: NextFunction) {
         const companyLoginSchema = Joi.object({
-            email:Joi.string().required(),
-            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required()
-        })
+            email: Joi.string().required(),
+            password: Joi.string()
+                .pattern(
+                    new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$'),
+                )
+                .required(),
+        });
 
-        const {error} = companyLoginSchema.validate(req.body);
+        const { error } = companyLoginSchema.validate(req.body);
 
-        if(error){
+        if (error) {
             next(error);
         }
 
-        const { email, password}:{email: string, password:string} = req.body;
+        const { email, password }: { email: string; password: string } =
+            req.body;
 
         try {
-            const companyInfo = await company.findOne({email});
-            if(!companyInfo){
+            const companyInfo = await company.findOne({ email });
+            if (!companyInfo) {
                 return next(CustomErrorHandler.wrongCredentials());
             }
-            const matchPassword = await bcrypt.compare(password, companyInfo.password);
-            if(!matchPassword){
+            const matchPassword = await bcrypt.compare(
+                password,
+                companyInfo.password,
+            );
+            if (!matchPassword) {
                 return next(CustomErrorHandler.wrongCredentials());
             }
-            const access_token = JwtService.sign({ 
-                id: companyInfo._id, 
-                role: "company"
+            const access_token = JwtService.sign({
+                id: companyInfo._id,
+                role: roles.COMPANY,
             });
 
-            return  res.status(200).json({ access_token });
+            return res
+                .status(200)
+                .json({
+                    access_token,
+                    name: companyInfo.name,
+                    role: companyInfo.role,
+                });
         } catch (error) {
-            next(error)
+            next(error);
         }
-    }
+    },
 
     // async login(req: Request, res: Response, next: NextFunction) {
     //     const userLoginSchema = Joi.object({
@@ -114,7 +133,6 @@ const loginController = {
     //     }
 
     // }
-
-}
+};
 
 export default loginController;
--- src/controller/auth/RegisterController.ts
+++ src/controller/auth/RegisterController.ts
@@ -1,185 +1,236 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 import path from 'path';
-import User from "../../model/User";
-import Company from "../../model/company";
+import User from '../../model/User';
+import Company from '../../model/company';
 import CustomErrorHandler from '../../services/customErrorHandeler';
 import bcrypt from 'bcrypt';
 import JwtService from '../../services/jwtServices';
 import { MulterService } from '../../services/multerService';
+import roles from '../../services/roleService';
 
 const registerController = {
-  //User Register Controller
+    //User Register Controller
 
-  async userRegister(req:Request, res:Response, next:NextFunction){
-      const userRegisterSchema = Joi.object({
-        name: Joi.string().min(5).max(30).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required(),
-        phone: Joi.string().min(10).required(),
-        workStatus: Joi.string().required(),
-        repeat_password: Joi.ref('password'),
-    });
-
-    const {error} = userRegisterSchema.validate(req.body);
-
-    if(error){
-        next(error);
-    }
-
-    try{
-        const exist = await User.exists({ email: req.body.email });
-        if(exist){
-            return next(CustomErrorHandler.alreadyExist("This email is already taken !"));
-        }
-    }catch(err){
-        next(err);
-    }
-
-    // user not in database register new
-
-    const {name, email, password, phone, workStatus}:{name:string, email:string, password:string, phone:string, workStatus:string}= req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-        name,
-        email,
-        password:hashedPassword,
-        phoneNo:phone,
-        workStatus,
-        role:"user"
-    });
-    let acc_token:any;
-    try {
-        const saveUser = await user.save();
-        //generate access token
-        acc_token = JwtService.sign({
-            id: saveUser._id,
-            role:"user"
+    async userRegister(req: Request, res: Response, next: NextFunction) {
+        const userRegisterSchema = Joi.object({
+            name: Joi.string().min(5).max(30).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string()
+                .pattern(
+                    new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$'),
+                )
+                .required(),
+            phone: Joi.string().min(10).required(),
+            workStatus: Joi.string().required(),
+            repeat_password: Joi.ref('password'),
         });
-    } catch (error) {
-        next(error);
-    }
 
-    res.status(200).json({acc_token:acc_token});
-  },
+        const { error } = userRegisterSchema.validate(req.body);
 
-
-
-  // //Company Register Controller
-
-
-  async companyRegister(req: Request , res: Response ,  next: NextFunction){
-      const comapanyRegisterSchema = Joi.object({
-        name: Joi.string().min(5).max(40).required(),
-        designation: Joi.string().min(10).required(),
-        email:Joi.string().email().required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required(),
-        repeatPassword: Joi.ref('password'),
-        phone:Joi.string().min(10).required(),
-        industry: Joi.string().required(),
-        companyName: Joi.string().required(),
-        pin:Joi.string().required(),
-        address: Joi.string().required(),
-        
-      });
-
-      console.log(req.body);
-
-      const {error} = comapanyRegisterSchema.validate(req.body);
-
-      if(error){
-        next(error);
-      }
-
-      //if company name present in the DataBase
-      try{
-        const exist = await Company.exists({email: req.body.email});
-        if(exist) {
-          return next(CustomErrorHandler.alreadyExist("This Company Email is already Exist!"));
+        if (error) {
+            next(error);
         }
-      }catch(err){
-        next(err);
-      }
 
-      //when company name is not present in the database
-      const {name , description , email , password , industry, designation, pin, address, companyName, phone}: {name: string , description: string, email: string , password: string , repeatPassword: string , industry:string, designation:string, pin:string, address:string, companyName:string, phone:string} = req.body;
+        try {
+            const exist = await User.exists({ email: req.body.email });
+            if (exist) {
+                return next(
+                    CustomErrorHandler.alreadyExist(
+                        'This email is already taken !',
+                    ),
+                );
+            }
+        } catch (err) {
+            next(err);
+        }
 
-      const hashPassword = await bcrypt.hash(password , 10);
-      const company = new Company({
-        name,
-        description,
-        email,
-        password : hashPassword,
-        companyName,
-        industry,
-        pin,
-        phone,
-        address,
-        designation,
-        role:"company"
-      });
-      let acc_token:any;
-      try{
-        const saveCompany = await company.save();
+        // user not in database register new
 
-        acc_token = JwtService.sign({
-          id: saveCompany._id,
-          role : "company"
+        const {
+            name,
+            email,
+            password,
+            phone,
+            workStatus,
+        }: {
+            name: string;
+            email: string;
+            password: string;
+            phone: string;
+            workStatus: string;
+        } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            phoneNo: phone,
+            workStatus,
+            role: 'user',
         });
-      } catch(error){
-        next(error);
-      }
+        let acc_token: any;
+        try {
+            const saveUser = await user.save();
+            //generate access token
+            acc_token = JwtService.sign({
+                id: saveUser._id,
+                role: 'user',
+            });
+        } catch (error) {
+            next(error);
+        }
 
-      res.status(200).json({access_token: acc_token});
+        res.status(200).json({ acc_token: acc_token });
     },
 
-  // async register(req: Request, res: Response, next: NextFunction) {
-  //   const registerSchema = Joi.object({
-  //     name: Joi.string().required(),
-  //     email: Joi.string().required(),
-  //     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required(),
-  //     role: Joi.string().required()
-  //   });
+    // //Company Register Controller
 
-  //   const { error } = registerSchema.validate(req.body);
-  //   if (error) {
-  //     next(error);
-  //   }
-  //   const { name, email, password, role }: { name: string, email: string, password: string, role: string } = req.body;
+    async companyRegister(req: Request, res: Response, next: NextFunction) {
+        const comapanyRegisterSchema = Joi.object({
+            name: Joi.string().min(5).max(40).required(),
+            designation: Joi.string().min(10).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string()
+                .pattern(
+                    new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$'),
+                )
+                .required(),
+            repeatPassword: Joi.ref('password'),
+            phone: Joi.string().min(10).required(),
+            industry: Joi.string().required(),
+            companyName: Joi.string().required(),
+            pin: Joi.string().required(),
+            address: Joi.string().required(),
+        });
 
-  //   try {
-  //     const exist = await User.exists({ email: req.body.email });
-  //     if (exist) {
-  //       return next(CustomErrorHandler.alreadyExist("User is already Exist!"));
-  //     }
-  //   } catch (err) {
-  //     next(err);
-  //   }
+        console.log(req.body);
 
-  //   const hashedPassword = await bcrypt.hash(password, 10);
+        const { error } = comapanyRegisterSchema.validate(req.body);
 
+        if (error) {
+            next(error);
+        }
 
-  //   const user = new User({
-  //     name,
-  //     email,
-  //     password: hashedPassword,
-  //     role
-  //   });
+        //if company name present in the DataBase
+        try {
+            const exist = await Company.exists({ email: req.body.email });
+            if (exist) {
+                return next(
+                    CustomErrorHandler.alreadyExist(
+                        'This Company Email is already Exist!',
+                    ),
+                );
+            }
+        } catch (err) {
+            next(err);
+        }
 
-  //   let acc_token:any;
+        //when company name is not present in the database
+        const {
+            name,
+            description,
+            email,
+            password,
+            industry,
+            designation,
+            pin,
+            address,
+            companyName,
+            phone,
+        }: {
+            name: string;
+            description: string;
+            email: string;
+            password: string;
+            repeatPassword: string;
+            industry: string;
+            designation: string;
+            pin: string;
+            address: string;
+            companyName: string;
+            phone: string;
+        } = req.body;
 
-  //   try {
-  //     const saveuser = await user.save();
-  //     acc_token = JwtService.sign({
-  //       id: saveuser.id,
-  //       role: saveuser.role
-  //     });
-  //     res.status(200).json({acc_token: acc_token});
-  //   } catch (error) {
-  //     next(error);
-  //   }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const company = new Company({
+            name,
+            description,
+            email,
+            password: hashPassword,
+            companyName,
+            industry,
+            pin,
+            phone,
+            address,
+            designation,
+            role: roles.COMPANY,
+        });
+        let acc_token: any;
+        try {
+            const saveCompany = await company.save();
 
-  // }
-}
+            acc_token = JwtService.sign({
+                id: saveCompany._id,
+                role: roles.COMPANY,
+            });
+        } catch (error) {
+            next(error);
+        }
+
+        res.status(200).json({
+            access_token: acc_token,
+            name,
+            role: roles.COMPANY,
+        });
+    },
+
+    // async register(req: Request, res: Response, next: NextFunction) {
+    //   const registerSchema = Joi.object({
+    //     name: Joi.string().required(),
+    //     email: Joi.string().required(),
+    //     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?~-]{3,30}$')).required(),
+    //     role: Joi.string().required()
+    //   });
+
+    //   const { error } = registerSchema.validate(req.body);
+    //   if (error) {
+    //     next(error);
+    //   }
+    //   const { name, email, password, role }: { name: string, email: string, password: string, role: string } = req.body;
+
+    //   try {
+    //     const exist = await User.exists({ email: req.body.email });
+    //     if (exist) {
+    //       return next(CustomErrorHandler.alreadyExist("User is already Exist!"));
+    //     }
+    //   } catch (err) {
+    //     next(err);
+    //   }
+
+    //   const hashedPassword = await bcrypt.hash(password, 10);
+
+    //   const user = new User({
+    //     name,
+    //     email,
+    //     password: hashedPassword,
+    //     role
+    //   });
+
+    //   let acc_token:any;
+
+    //   try {
+    //     const saveuser = await user.save();
+    //     acc_token = JwtService.sign({
+    //       id: saveuser.id,
+    //       role: saveuser.role
+    //     });
+    //     res.status(200).json({acc_token: acc_token});
+    //   } catch (error) {
+    //     next(error);
+    //   }
+
+    // }
+};
 
 export default registerController;
