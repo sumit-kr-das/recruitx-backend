99562bb2a8f93ecc9b33b5c12ab78b2f79ed77cb
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-22 01:41:04 +0530

    cors policy changed

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173/', credentials: true }));
 
 /* all routes */
 app.use('/', testRoutes);
