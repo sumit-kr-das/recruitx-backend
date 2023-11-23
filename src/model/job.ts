410dc07629a9a50ac5572a3dd4eb4b24edd47f03
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-23 19:47:18 +0530

    job post controller location not defined problem solved

--- src/model/job.ts
+++ src/model/job.ts
@@ -1,54 +1,63 @@
-import { Schema, model, Document } from "mongoose";
-import company from "./company";
+import { Schema, model, Document } from 'mongoose';
+import company from './company';
 
-export interface IJob extends Document{
-    title:string,
-    category:string,
-    description:string,
-    tags:[string],
-    active:boolean,
-    companyId:string,
-    info:{
-        vacancies:number,
-        type:string,
-        startDate:Date,
-        endDate:Date,
-        roles:string,
-        skills:[string],
-        minExprience:number,
-        maxExprience?:number,
-        minSalary?:number,
-        maxSalary?:number,
-        location?:string,
-        maxQualification: string,
-        degree:[string]
-    },
+export interface IJob extends Document {
+    title: string;
+    category: string;
+    description: string;
+    tags: [string];
+    active: boolean;
+    companyId: string;
+    info: {
+        vacancies: number;
+        jobType: string;
+        workplaceType: string;
+        startDate: Date;
+        endDate: Date;
+        roles: string;
+        skills: [string];
+        minExprience: number;
+        maxExprience?: number;
+        minSalary?: number;
+        maxSalary?: number;
+        location?: string;
+        maxQualification: string;
+        degree: string;
+    };
 }
 
-const JobSchema:Schema = new Schema({
-    title:{type:String, required:true},
-    category:{type:String, required:true},
-    description:{type:String, required:true},
-    tags:{type:Array, required:true},
-    active:{type:Boolean, required:true},
-    companyId:{type:Schema.Types.ObjectId, ref:"Company", required:true},
-    info:{
-        vacancies:{type:Number, required:true},
-        type:{type:String, required:true},
-        startDate:{type:Date, required:true},
-        endDate:{type:Date, required:true},
-        roles:{type:String, required:true},
-        skills:{type:Array, required:true},
-        minExprience:{type:Number, required:true},
-        maxExprience:{type:Number},
-        minSalary:{type:Number},
-        maxSalary:{type:Number},
-        location:{type:String},
-        maxQualification: {type:String, required:true},
-        degree: {type:Array, required:true}
+const JobSchema: Schema = new Schema(
+    {
+        title: { type: String, required: true },
+        category: { type: String, required: true },
+        description: { type: String, required: true },
+        tags: { type: Array, required: true },
+        active: { type: Boolean, required: true },
+        companyId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Company',
+            required: true,
+        },
+        info: {
+            vacancies: { type: Number, required: true },
+            jobType: { type: String, required: true },
+            workplaceType: { type: String, required: true },
+            startDate: { type: Date, required: true },
+            endDate: { type: Date, required: true },
+            roles: { type: String, required: true },
+            skills: { type: Array, required: true },
+            minExprience: { type: Number, required: true },
+            maxExprience: { type: Number },
+            minSalary: { type: Number },
+            maxSalary: { type: Number },
+            location: { type: String },
+            maxQualification: { type: String, required: true },
+            degree: { type: String, required: true },
+        },
     },
-},{
-    timestamps:true
-});
+    {
+        timestamps: true,
+    },
+);
 
-export default model<IJob>("Job",JobSchema);
+export default model<IJob>('Job', JobSchema);
