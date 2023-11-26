3d1f648804be24b8dcbc8b84553716aec3ae0d11
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-26 11:42:33 +0530

    internal server error

--- src/controller/user/UserinfoController.ts
+++ src/controller/user/UserinfoController.ts
@@ -1,46 +1,45 @@
-import { Response, NextFunction } from "express";
-import Joi from "joi";
-import userInfo from "../../model/userInfo";
+import { Response, NextFunction } from 'express';
+import Joi from 'joi';
+import userInfo from '../../model/userInfo';
 
 const userInfoController = {
-    async addUseInfo(req:any, res:Response, next:NextFunction){
+    async addUseInfo(req: any, res: Response, next: NextFunction) {
         const userId = req.user.id;
-        const data = await userInfo.find({userId});
-        if(data){
-            return res.status(503).json({msg:"User information already exists"});
+        const data = await userInfo.find({ userId });
+        if (data) {
+            return res
+                .status(503)
+                .json({ msg: 'User information already exists' });
         }
+        console.log(req.file)
         const userinfoSchema = Joi.object({
-            phone: Joi.string().min(10).required(),
             github: Joi.string().required(),
             linkedIn: Joi.string(),
             dateOfBirth: Joi.date().required(),
-            age:Joi.number().required(),
+            age: Joi.number().required(),
             address: Joi.string().required(),
             bio: Joi.string().required(),
-            objective : Joi.string().required(),
+            objective: Joi.string().required(),
             status: Joi.string().required(),
             language: Joi.array().required(),
             gender: Joi.string().required(),
             skills: Joi.array().required(),
-            maxQualification: Joi.string().required()
+            maxQualification: Joi.string().required(),
         });
 
-        const {error} = userinfoSchema.validate(req.body);
+        const { error } = userinfoSchema.validate(req.body);
 
-        if(error){
+        if (error) {
             next(error);
         }
 
         const photo = req.file;
 
-        if(!photo){
-            return res.status(503).json({msg:"Please enter photo"});
+        if (!photo) {
+            return res.status(503).json({ msg: 'Please enter photo' });
         }
 
-        const {phone, github, linkedIn, dateOfBirth, age, address, bio, objective, status, language, gender, skills, maxQualification}:{phone:string, github:string, linkedIn:string, dateOfBirth:Date, age:number, address:string, bio:string, objective:string, status:string, language:[string], gender:string, skills:[string], maxQualification:string} = req.body;
-
-        const newUserinfo = new userInfo({
-            phone,
+        const {
             github,
             linkedIn,
             dateOfBirth,
@@ -52,34 +51,63 @@ const userInfoController = {
             language,
             gender,
             skills,
-            photo:photo?.path,
+            maxQualification,
+        }: {
+            github: string;
+            linkedIn: string;
+            dateOfBirth: Date;
+            age: number;
+            address: string;
+            bio: string;
+            objective: string;
+            status: string;
+            language: [string];
+            gender: string;
+            skills: [string];
+            maxQualification: string;
+        } = req.body;
+
+        const newUserinfo = new userInfo({
+            github,
+            linkedIn,
+            dateOfBirth,
+            age,
+            address,
+            bio,
+            objective,
+            status,
+            language,
+            gender,
+            skills,
+            photo: photo?.path,
             userId,
-            maxQualification
+            maxQualification,
         });
 
         try {
-            const saveInfo = await newUserinfo.save();
-            return res.status(200).json({msg:"Your informations saved successfully"});
+            await newUserinfo.save();
+            return res
+                .status(200)
+                .json({ msg: 'Your informations saved successfully' });
         } catch (error) {
             next(error);
         }
     },
 
-    async viewUserinfo(req:any, res:Response, next:NextFunction){
+    async viewUserinfo(req: any, res: Response, next: NextFunction) {
         const userId = req.user.id;
         console.log(userId);
         try {
-          const info = await userInfo.find({userId:userId});
-          console.log(info);
-          return res.status(200).json(info);
+            const info = await userInfo.find({ userId: userId });
+            console.log(info);
+            return res.status(200).json(info);
         } catch (error) {
-          next(error);
+            next(error);
         }
     },
 
-    async updateUserinfo(req:any, res:Response, next:NextFunction){
+    async updateUserinfo(req: any, res: Response, next: NextFunction) {
         const userId = req.user.id;
-
 
         const userinfoSchema = Joi.object({
             phone: Joi.string().min(10).required(),
@@ -96,15 +124,15 @@ const userInfoController = {
             skills: Joi.array().required(),
             maxQualification: Joi.string().required(),
         });
-    
+
         const { error } = userinfoSchema.validate(req.body);
-    
+
         if (error) {
             next(error);
         }
-    
+
         const photo = req.file;
-    
+
         const {
             phone,
             github,
@@ -118,7 +146,7 @@ const userInfoController = {
             language,
             gender,
             skills,
-            maxQualification
+            maxQualification,
         }: {
             phone: string;
             github: string;
@@ -132,9 +160,9 @@ const userInfoController = {
             language: [string];
             gender: string;
             skills: [string];
-            maxQualification: string
+            maxQualification: string;
         } = req.body;
-    
+
         const updateFields: {
             phone: string;
             github: string;
@@ -149,7 +177,7 @@ const userInfoController = {
             gender: string;
             skills: [string];
             photo?: string;
-            maxQualification: string
+            maxQualification: string;
         } = {
             phone,
             github,
@@ -163,26 +191,34 @@ const userInfoController = {
             language,
             gender,
             skills,
-            maxQualification
+            maxQualification,
         };
-    
+
         if (photo) {
             updateFields.photo = photo.path;
         }
-    
+
         try {
-            const updatedInfo = await userInfo.findOneAndUpdate({userId}, updateFields, { new: true });
-    
+            const updatedInfo = await userInfo.findOneAndUpdate(
+                { userId },
+                updateFields,
+                { new: true },
+            );
+
             if (!updatedInfo) {
-                return res.status(404).json({ msg: "User information not found" });
+                return res
+                    .status(404)
+                    .json({ msg: 'User information not found' });
             }
-    
-            return res.status(200).json({ msg: "Your information updated successfully", data: updatedInfo });
+
+            return res.status(200).json({
+                msg: 'Your information updated successfully',
+                data: updatedInfo,
+            });
         } catch (error) {
             next(error);
         }
     },
-
-}
+};
 
 export default userInfoController;
