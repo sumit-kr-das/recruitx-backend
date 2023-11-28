e553535878ca9dc1515648d019453d61faab63ec
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-28 15:32:56 +0530

    UserExprienceSchema new properties added

--- src/model/userExprience.ts
+++ src/model/userExprience.ts
@@ -1,28 +1,34 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document } from 'mongoose';
 
-export interface IUserExprience extends Document{
-    userId:string,
-    skills:[string],
-    companyName:string,
-    duration:{
-        startDate:Date,
-        endDate?:Date,
-    }
-    place:string
+export interface IUserExprience extends Document {
+    userId: string;
+    skills: [string];
+    companyName: string;
+    designation: string,
+    experience: string,
+    annualSalary: string,
+    type: string,
+    startDate: Date;
+    endDate?: Date;
+    jobProfile: string;
 }
 
-const UserExprienceSchema:Schema = new Schema({
-    userId:{type:String, required:true},
-    skills:{type:Array, required:true},
-    companyName:{type:String, required:true},
-    duration:{
-        startDate:{type:Date, required:true},
-        endDate:{type:Date}
+const UserExprienceSchema: Schema = new Schema(
+    {
+        userId: { type: String, required: true },
+        skills: { type: Array, required: true },
+        companyName: { type: String, required: true },
+        designation: { type: String, required: true },
+        experience: { type: Number, required: true },
+        annualSalary: { type: Number, required: true },
+        type: { type: String, required: true },
+        startDate: { type: Date, required: true },
+        endDate: { type: Date },
+        jobProfile: { type: String, require: true },
     },
-    place:{type:String, require:true}
-},{
-    timestamps:true
-})
+    {
+        timestamps: true,
+    },
+);
 
-
-export default model<IUserExprience>("UserExprience",UserExprienceSchema);
+export default model<IUserExprience>('UserExprience', UserExprienceSchema);
