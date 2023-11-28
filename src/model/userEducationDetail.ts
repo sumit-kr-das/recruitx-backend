ae0407093c7874ed9eb4333a7cda27dd7a643b8a
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-28 15:24:27 +0530

    userEducationDetail model new properties added

--- src/model/userEducationDetail.ts
+++ src/model/userEducationDetail.ts
@@ -1,27 +1,33 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document } from 'mongoose';
 
-export interface IUserEducationDetail extends Document{
-    userId:string,
-    degree:string,
-    college:string,
-    duration:{
-        admissionYear:number,
-        passYear:number
-    },
-    marks:number
+export interface IUserEducationDetail extends Document {
+    userId: string;
+    degree: string;
+    college: string;
+    course: string;
+    courseType: string;
+    admissionYear: number;
+    passYear: number;
+    marks: number;
 }
 
-const UserEducationDetailSchema:Schema = new Schema({
-    userId:{type:String, required:true},
-    degree:{type:String, required:true},
-    college:{type:String, required:true},
-    duration:{
-        admissionYear:{type:Number, required:true},
-        passYear:{type:Number, required:true},
+const UserEducationDetailSchema: Schema = new Schema(
+    {
+        userId: { type: String, required: true },
+        degree: { type: String, required: true },
+        college: { type: String, required: true },
+        course: { type: String, required: true },
+        courseType: { type: String, required: true },
+        admissionYear: { type: Number, required: true },
+        passYear: { type: Number, required: true },
+        marks: { type: Number, required: true },
     },
-    marks:{type:Number, required:true}
-},{
-    timestamps:true,
-});
+    {
+        timestamps: true,
+    },
+);
 
-export default model<IUserEducationDetail>("UserEducationDetail",UserEducationDetailSchema);
+export default model<IUserEducationDetail>(
+    'UserEducationDetail',
+    UserEducationDetailSchema,
+);
