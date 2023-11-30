686edb04b3c1ad937d93cc75c637f97f08621490
Shoyeb001 <shoyebjio3398@gmail.com>
2023-11-30 15:00:50 +0530

    recomandation fixed

--- src/controller/job/JobRecomandationController.ts
+++ src/controller/job/JobRecomandationController.ts
@@ -8,15 +8,16 @@ import userInfo from "../../model/userInfo";
 const jobRecomandationController = {
     async recommendJobs(req: any, res: Response, next: NextFunction) {
         const userId = req.user.id;
-        const hasinfo = req.query.hasinfo;
+        const hasinfo = req.query.hasInfo;
         const limit = req.query.limit;
         const currentDate = new Date();
+        console.log(hasinfo);
         try {
-            if (hasinfo) {
+            if (hasinfo === "true") {
                 const userSkills: any = await userInfo.find({ userId });
                 const skill = userSkills[0]?.skills;
+                console.log(skill);
                 const jobs = await job.find({
-
                     'info.skills': {
                         $elemMatch: {
                             $in: skill,
@@ -25,7 +26,7 @@ const jobRecomandationController = {
                     active: true,
                     'info.startDate': { $lte: currentDate },
                     'info.endDate': { $gte: currentDate },
-                }).limit(limit);
+                }).limit(limit).populate("companyId", "companyName");
                 return res.status(200).json(jobs);
             } else {
                 const jobs = await job.find({
